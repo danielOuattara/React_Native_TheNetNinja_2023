5f3a1c189b6b980c13562a31a49fa24024db92bb
danielOuattara <daniel.ouattara@gmx.com>
2023-09-15 23:09:20 +0200

    07 FlatList

---
+++ 07_Flat_List_Component/App.jsx
@@ -0,0 +1,66 @@
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  FlatList,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+
+export default function App() {
+  const [people, setPeople] = useState([
+    { name: "shaun", id: "1" },
+    { name: "yoshi", id: "2" },
+    { name: "mario", id: "3" },
+    { name: "luigi", id: "4" },
+    { name: "peach", id: "5" },
+    { name: "toad", id: "6" },
+    { name: "bowser", id: "7" },
+    { name: "rabbit", id: "8" },
+    { name: "wolf", id: "9" },
+    { name: "pig", id: "10" },
+  ]);
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={people}
+        numColumns={2}
+        keyExtractor={(item) => item.id}
+        renderItem={({ index, item }) => (
+          <Text style={styles.person}>{item.name}</Text>
+        )}
+      />
+
+      {/* <ScrollView>
+        {people.map((person) => (
+          <View key={person.id}>
+            <Text style={styles.person}>{person.name}</Text>
+          </View>
+        ))}
+      </ScrollView> */}
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#d0eebf",
+    paddingTop: 40,
+    paddingHorizontal: 25,
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+  person: {
+    marginTop: 24,
+    padding: 20,
+    backgroundColor: "green",
+    fontSize: 24,
+    borderRadius: 6,
+    marginHorizontal: 10,
+  },
+});
