c3872198f60527e2c4ffb9f0bf3cfb87d4d75b13
danielOuattara <daniel.ouattara@gmx.com>
2023-09-18 20:01:53 +0200

    09 todos app part 1

---
+++ 09_todo_app_part_1/App.jsx
@@ -0,0 +1,48 @@
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import Header from "./components/Header";
+
+export default function App() {
+  const [todos, setTodos] = useState([
+    { text: "buy coffee", key: "1" },
+    { text: "create an app", key: "2" },
+    { text: "paly football", key: "3" },
+  ]);
+  return (
+    <View style={styles.container}>
+      <Header />
+      <View style={styles.content}>
+        {/* to do form */}
+
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => <Text>{item.text}</Text>}
+          />
+        </View>
+      </View>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  content: {
+    padding: 40,
+  },
+
+  list: {
+    marginTop: 20,
+  },
+  header: {
+    height: 80,
+    paddingTop: 38,
+    backgroundColor: "coral",
+  },
+});
