ae492ccb2b6637cb7e61e3ab55a392b5410cb93a
danielOuattara <daniel.ouattara@gmx.com>
2023-09-19 23:38:24 +0200

    23 navigation options

---
+++ 23_navigation_options/routes/homeStackNavigation.jsx
@@ -0,0 +1,31 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Home, ReviewDetails } from "./../screens";
+
+const Stack = createNativeStackNavigator();
+
+export default function HomeStackNavigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerTintColor: "white",
+          headerStyle: { backgroundColor: "tomato" },
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ title: "Game Zone" }}
+        />
+        <Stack.Screen
+          name="ReviewDetails"
+          component={ReviewDetails}
+          options={{ title: "Review Details" }}
+          initialParams={{ countryOfOrigin: "China" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
