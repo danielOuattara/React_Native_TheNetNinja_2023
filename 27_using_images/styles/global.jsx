866f595e4cd482b88052db60511644cb9de60dee
danielOuattara <daniel.ouattara@gmx.com>
2023-09-21 10:54:13 +0200

    27 using images OR stars component evaluation

---
+++ 27_using_images/styles/global.jsx
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+export const globalStyles = StyleSheet.create({
+  container: {
+    padding: 20,
+    flex: 1,
+  },
+  titleText: {
+    fontFamily: "nunito-bold",
+    fontSize: 28,
+    color: "#333",
+  },
+  titleText2: {
+    fontFamily: "Montserrat_400Regular",
+    fontSize: 28,
+    color: "#333",
+  },
+  paragraph: {
+    marginVertical: 8,
+    lineHeight: 20,
+  },
+});
